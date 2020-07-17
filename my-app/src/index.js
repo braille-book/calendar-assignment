3568dae1b13a08949b67f1f702fa7c32b0690de8
M1cha3lJ0hns0n <mj66@hawaii.edu>
2020-07-16 20:38:54 -1000

    Update The Form Again

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,7 +9,36 @@ const ampm = [
   {key:'AM', text:'AM', value:'AM'},
   {key:'PM', text:'PM', value:'PM'},
 ]
+
 class Calendar extends React.Component {
+  downloadIcsFile = () => {
+    const element = document.createElement("a");
+
+    let text =  'BEGIN:VCALENDAR\n' +
+        'CALSCALE:GREGORIAN\n' +
+        'BEGIN:VEVENT\n' +
+        'DTSTART:' + document.getElementById('beginDate').value.replace(/-/g, '') + 'T' +
+                    document.getElementById('beginTime').value.replace(/:/g, '') + '00\n' +
+        'DTEND:' + document.getElementById('endDate').value.replace(/-/g, '') + 'T' +
+                    document.getElementById('endTime').value.replace(/:/g, '') + '00\n' +
+        'DTSTAMP:20200630T051242Z\n' +
+        'UID:'+ (Math.floor(100000 + Math.random() * 900000)) + '0-E749-430B-8CAF-0E4F40551615\n' +
+        'CREATED:1212312T213424234\n' +
+        'LAST-MODIFIED:20200630T051241Z\n' +
+        'LOCATION:' + document.getElementById('loc').value + '\n' +
+        'SUMMARY:' + document.getElementById('summ').value + '\n' +
+        'END:EVENT\n' +
+        'END:CALENDAR\n'
+
+
+    const file = new Blob([text], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "myEvent.ics";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
+
 
   render() {
     return (
@@ -21,38 +50,37 @@ class Calendar extends React.Component {
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Start Date For Event</label>
-          <Input placeholder="e.g     MM/DD/YYYY" />
-
+          <Input id='beginDate' type='date' placeholder="e.g     MM/DD/YYYY" />
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Start Time For Event</label>
-          <Input label={<Dropdown defaultValue="AM" options={ampm}/>} labelPosition='right' placeholder="e.g    HH:MM" />
+          <Input id='beginTime' type='time' placeholder="e.g    HH:MM" />
     </div>
     </Form>
          <Form class="ui form">
            <div class="field">
              <label>Please Enter End Date For Event</label>
-             <input placeholder="e.g     MM/DD/YYYY" />
+             <input id='endDate' type='date'  placeholder="e.g     MM/DD/YYYY" />
            </div>
          </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter End Time For Event</label>
-          <Input label={<Dropdown defaultValue="AM" options={ampm}/>} labelPosition='right' placeholder="e.g    HH:MM" />
+          <Input id='endTime' type='time' labelPosition='right' placeholder="e.g    HH:MM" />
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Description For Event</label>
-    <input placeholder="Please Enter the Description For the Event" />
+    <input id='summ' placeholder="Please Enter the Description For the Event" />
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Location For Event</label>
-    <input placeholder="Please Enter the Location For the Event" />
+    <input id={'loc'} placeholder="Please Enter the Location For the Event" />
     </div>
     </Form>
     <Form class="ui form">
@@ -111,8 +139,8 @@ class Calendar extends React.Component {
         <div class="ui input"><input type="text" placeholder="e.g joe@schmoe.com" /></div>
         </div>
     </Form>
-      <div class='buttons'>
-    <button class="ui button">Submit</button>
+      <div id='submit' class='buttons'>
+    <button class="ui button" onClick={this.downloadIcsFile}>Submit</button>
       </div>
 
        </Grid.Column>
