7cf82afa4512e69f68a019bdc2bdb3ca3b801988
M1cha3lJ0hns0n <mj66@hawaii.edu>
2020-07-16 18:19:00 -1000

    Updated the Form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import DatePicker from "react-datepicker";
-import TimePicker from "react-time-picker";
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './style.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { Container, Header,Form,Grid } from 'semantic-ui-react';
+import { Container, Header, Form, Grid, Dropdown, Input } from 'semantic-ui-react';
 
+const ampm = [
+  {key:'AM', text:'AM', value:'AM'},
+  {key:'PM', text:'PM', value:'PM'},
+]
 class Calendar extends React.Component {
-  state = {
-    startDate: new Date(),
-
-  };
-
-  onChangeB = time1 => this.setState({ time1 })
-  onChangeE = time2 => this.setState({ time2 })
-
-  beginDate = date1 => {
-    this.setState({
-      startDate: date1
-    });
-  };
-
-  endDate = date2 => {
-    this.setState({
-      endDate: date2
-    });
-  };
 
   render() {
     return (
@@ -38,49 +21,38 @@ class Calendar extends React.Component {
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Start Date For Event</label>
-          <DatePicker
-              selected={this.state.startDate}
-              onChange={this.beginDate}
-          />
+          <Input placeholder="e.g     MM/DD/YYYY" />
+
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Start Time For Event</label>
-          <TimePicker
-              onChange={this.onChangeB}
-              value={this.state.time1}
-          />
+          <Input label={<Dropdown defaultValue="AM" options={ampm}/>} labelPosition='right' placeholder="e.g    HH:MM" />
     </div>
     </Form>
          <Form class="ui form">
            <div class="field">
              <label>Please Enter End Date For Event</label>
-             <DatePicker
-                 selected={this.state.endDate}
-                 onChange={this.endDate}
-             />
+             <input placeholder="e.g     MM/DD/YYYY" />
            </div>
          </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter End Time For Event</label>
-          <TimePicker
-              onChange={this.onChangeE}
-              value={this.state.time2}
-          />
+          <Input label={<Dropdown defaultValue="AM" options={ampm}/>} labelPosition='right' placeholder="e.g    HH:MM" />
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Description For Event</label>
-    <input placeholder="Please Enter the Description For Event" />
+    <input placeholder="Please Enter the Description For the Event" />
     </div>
     </Form>
     <Form class="ui form">
         <div class="field">
         <label>Please Enter Location For Event</label>
-    <input placeholder="Please Enter the Location For Event" />
+    <input placeholder="Please Enter the Location For the Event" />
     </div>
     </Form>
     <Form class="ui form">
@@ -89,13 +61,60 @@ class Calendar extends React.Component {
     <input placeholder="Please Enter the Guest Email" />
     </div>
     </Form>
+       <Form>
+         <div className="ui form">
+           <div className="groupedfields">
+             <label>Would you like to RSVP?</label>
+             <div className="field">
+               <div className="ui radio checkbox">
+                 <input type="radio" name="example2" checked="checked"/>
+                   <label>Yes</label>
+               </div>
+             </div>
+             <div className="field">
+               <div className="ui radio checkbox">
+                 <input type="radio" name="example2"/>
+                   <label>No</label>
+               </div>
+             </div>
+           </div>
+         </div>
+       </Form>
+         <Form>
+         <div className="ui form">
+           <div className="groupedfields">
+             <label>Would you like to make this Event Public, Private, or Confidential?</label>
+             <div className="field">
+               <div className="ui radio checkbox">
+                 <input type="radio" name="example3" checked="checked"/>
+                 <label>Public</label>
+               </div>
+             </div>
+             <div className="field">
+               <div className="ui radio checkbox">
+                 <input type="radio" name="example3"/>
+                 <label>Private</label>
+               </div>
+             </div>
+             <div className="field">
+               <div className="ui radio checkbox">
+                 <input type="radio" name="example3" checked="checked"/>
+                 <label>Confidential</label>
+               </div>
+             </div>
+           </div>
+         </div>
+         </Form>
     <Form class="ui success form">
         <div class="field">
         <label>Email</label>
-        <div class="ui input"><input type="text" placeholder="joe@schmoe.com" /></div>
+        <div class="ui input"><input type="text" placeholder="e.g joe@schmoe.com" /></div>
         </div>
+    </Form>
+      <div class='buttons'>
     <button class="ui button">Submit</button>
-        </Form>
+      </div>
+
        </Grid.Column>
      </Grid.Row>
      </Grid>
